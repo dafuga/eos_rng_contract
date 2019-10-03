f1c8553cfb2499cf612e7d3e8b052f73e988618a
dafuga <danielfugere28@gmail.com>
2019-10-03 16:53:18 +0900

    fix: got the contract publishing again

--- greymass.cpp
+++ greymass.cpp
@@ -12,13 +12,13 @@ public:
   greymass(name receiver, name code,  datastream<const char*> ds): contract(receiver, code, ds) {}
 
   [[eosio::action]]
-  void upsertnumber(name user, std::string hash, std::string revealed_number) {
+  void upsertnumber(name user, eosio::checksum256 hash, uint32_t revealed_number) {
     require_auth(user);
-    rnumber_index rnumbers(get_first_receiver(), get_first_receiver().value);
-    auto iterator = rnumbers.find(user.value);
-    if( iterator == rnumbers.end() )
+    committed_r_number_index committed_r_numbers(get_first_receiver(), get_first_receiver().value);
+    auto iterator = committed_r_numbers.find(user.value);
+    if( iterator == committed_r_numbers.end() )
     {
-      rnumbers.emplace(user, [&]( auto& row ) {
+      committed_r_numbers.emplace(user, [&]( auto& row ) {
        row.key = user;
        row.hash = hash;
        row.reveal_time = (eosio::current_time_point().sec_since_epoch() + 10);
@@ -32,14 +32,19 @@ public:
         return;
       }
 
-      // will raise an error if invalid hash
-      assert_sha256(iterator -> reveal, 3, iterator -> hash );
+      std::string revealed_number_string = std::to_string(iterator -> revealed_number);
+      char const *revealed_number_char = revealed_number_string.c_str();
 
-      rnumbers.modify(iterator, user, [&]( auto& row ) {
-        row.reveal = reveal;
+      // will raise an error if invalid hash
+      assert_sha256(revealed_number_char, 3, iterator -> hash );
+
+      committed_r_numbers.modify(iterator, user, [&]( auto& row ) {
+        row.revealed_number = revealed_number;
       });
 
-      modify_global_random_number(user, revealed_number)
+      int closest_block = iterator -> reveal_time / 500;
+
+      modify_global_random_number(user, revealed_number, closest_block);
     }
   }
 
@@ -47,66 +52,70 @@ public:
   void erase(name user) {
     require_auth(user);
 
-    rnumber_index rnumbers(get_first_receiver(), get_first_receiver().value);
+    committed_r_number_index committed_r_numbers(get_first_receiver(), get_first_receiver().value);
 
-    auto iterator = rnumbers.find(user.value);
-    check(iterator != rnumbers.end(), "Record does not exist");
-    rnumbers.erase(iterator);
+    auto iterator = committed_r_numbers.find(user.value);
+    check(iterator != committed_r_numbers.end(), "Record does not exist");
+    committed_r_numbers.erase(iterator);
   }
 
 private:
-  struct [[eosio::table]] random_number {
+  struct [[eosio::table]] committed_r_numbers {
     name key;
     eosio::checksum256 hash;
-    uint64_t block_number;
-    std::string reveal;
+    uint64_t reveal_time;
+    uint32_t revealed_number;
 
     uint64_t primary_key() const { return key.value; }
-    uint64_t get_secondary_1() const { return block_number;}
+    uint64_t get_reveal_time() const { return reveal_time; }
   };
 
-
-  struct [[eosio::table]] global_random_number {
+  struct [[eosio::table]] shared_r_numbers {
     uint64_t time_unit;
-    uint64_t global_random_value;
+    uint64_t closest_block;
+    uint32_t random_number;
   
     uint64_t primary_key() const { return time_unit; }
-    uint64_t get_random_number() const { return global_random_value; }
+    uint64_t get_random_number() const { return random_number; }
   };
 
+  void modify_global_random_number(name user, int random_number, int closest_block) {
+    shared_r_number_index shared_r_numbers(get_first_receiver(), get_first_receiver().value);
 
-  void modify_global_random_number(random_number) {
-    gnumber_index rnumbers(get_first_receiver(), get_first_receiver().value);
+    auto iterator = shared_r_numbers.find(user.value);
 
-    auto iterator = gnumbers.find(user.value);
+    if (iterator != shared_r_numbers.end()) {
+      shared_r_numbers.emplace(user, [&]( auto& row ) {
+        row.random_number = random_number;
+        row.closest_block = closest_block;
+      });
+    } else {
+      uint64_t new_random_number = computeXOR(iterator -> random_number, random_number);
 
-    uint64_t new_random_number = computeXOR(iterator -> reveal_time);
-
-    gnumbers.modify(iterator, self, [&]( auto& row ) {
-      row.random_number = new_random_number;
-    });
+      shared_r_numbers.modify(iterator, _self, [&]( auto& row ) {
+        row.random_number = new_random_number;
+      });
+    }
   };
 
-
-  typedef eosio::multi_index<"rnumbers"_n, rnumber> rnumber_index;
-  typedef eosio::multi_index<"gnumbers"_n, gnumber> gnumber_index;
-};
-
-uint64_t computeXOR(uint64_t x, uint64_t y) 
-{ 
+  uint64_t computeXOR(uint64_t x, uint64_t y) 
+  { 
     uint64_t res = 0;
       
-    for (int i = 31; i >= 0; i--)                      
-    { 
+    for (int i = 31; i >= 0; i--) { 
        // Find current bits in x and y 
-       bool b1 = x & (1 << i); 
-       bool b2 = y & (1 << i); 
+      bool b1 = x & (1 << i); 
+      bool b2 = y & (1 << i); 
          
-        // If both are 1 then 0 else xor is same as OR 
-        bool xoredBit = (b1 & b2) ? 0 : (b1 | b2);           
-  
-        res <<= 1; 
-        res |= xoredBit; 
+      // If both are 1 then 0 else xor is same as OR 
+      bool xoredBit = (b1 & b2) ? 0 : (b1 | b2);           
+
+      res <<= 1; 
+      res |= xoredBit; 
     } 
     return res; 
-} 
+  } 
+
+  typedef eosio::multi_index<"cnumbers"_n, committed_r_numbers> committed_r_number_index;
+  typedef eosio::multi_index<"snumbers"_n, shared_r_numbers> shared_r_number_index;
+};
